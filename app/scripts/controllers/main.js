53404ac247fbe9935577d16d97784a7fb9948420
William Lepinski <willsp@gmail.com>
2013-10-15 21:17:52 -0300

    Giving some style to the home page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,8 +3,9 @@
 angular.module('yiiApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+      { title: 'HTML5 Boilerplate', url: 'http://html5boilerplate.com/' },
+      { title: 'AngularJS', url: 'http://angularjs.org/' },
+      { title: 'Karma', url: 'http://karma-runner.github.io/0.10/index.html' },
+      { title: 'Yii Framework', url: 'http://yiiframework.com' }
     ];
   });
